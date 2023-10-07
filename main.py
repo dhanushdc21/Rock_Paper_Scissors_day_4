23be583ef5c4f98b0a44478611d44bfdbd7d639b
dhanushdc21 <131760172+dhanushdc21@users.noreply.github.com>
2023-10-07 22:17:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -25,27 +25,26 @@ scissors = '''
       (____)
 ---.__(___)
 '''
+attack=[rock,paper,scissors]
 while True:
  print("What do you choose? Type Rock, Paper or Scissors")
  choice_initial=input()
  choice=choice_initial.lower()
- if choice[0]=="r":
-   print(rock)
- elif choice[0]=="p":
-   print(paper)
- elif choice[0]=="s":
-   print(scissors)
+ human_choice=["r","p","s"]
+ if choice[0]==human_choice[0] or choice[0]==human_choice[1] or choice[0]==human_choice[2]:
+   print(attack[human_choice.index(choice[0])])#index=human_choice.index(choice[0])  This is the code to find the index position.
+ # if choice[0]=="r":
+ #   print(rock)
+ # elif choice[0]=="p":
+ #   print(paper)
+ # elif choice[0]=="s":
+ #   print(scissors)
  else:
    print("You have to choose from the three options only. Thats the game.")
    break
  print("Computer's choice :\n")
  comp_choice=random.randint(0,2)
- if comp_choice==0:
-   print(rock)
- elif comp_choice==1:
-   print(paper)
- else:
-   print(scissors)
+ print(attack[comp_choice])
  if ((choice[0]=="r" and comp_choice==2) or (choice[0]=="p" and comp_choice==0) or (choice[0]=="s" and comp_choice==1)):
    print("You Win!!!")
  elif((choice[0]=="r" and comp_choice==0) or (choice[0]=="p" and comp_choice==1) or (choice[0]=="s" and comp_choice==2)):
